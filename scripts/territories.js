6ae9375ca98abc89275ede50437e44e1d5e47baf
teetee971 <thierrynaud2009@gmail.com>
2025-11-13 11:00:09 -0400

    Update territories.js

--- scripts/territories.js
+++ scripts/territories.js
@@ -1,37 +1,63 @@
-export const TERRITORIES = {
-  guadeloupe: {
-    code: "guadeloupe",
+// Liste complète des territoires supportés
+export const TERRITORIES = [
+  {
+    id: "guadeloupe",
     name: "Guadeloupe",
-    lat: 16.265,
-    lon: -61.551,
-    zoom: 11
+    flag: "🇬🇵",
+    center: { lat: 16.265, lon: -61.551 }
   },
-  martinique: {
-    code: "martinique",
+  {
+    id: "martinique",
     name: "Martinique",
-    lat: 14.6415,
-    lon: -61.0242,
-    zoom: 11
+    flag: "🇲🇶",
+    center: { lat: 14.6415, lon: -61.0242 }
   },
-  guyane: {
-    code: "guyane",
+  {
+    id: "guyane",
     name: "Guyane",
-    lat: 4.9227,
-    lon: -52.3269,
-    zoom: 8
+    flag: "🇬🇫",
+    center: { lat: 4.9224, lon: -52.3135 }
   },
-  reunion: {
-    code: "reunion",
-    name: "La Réunion",
-    lat: -21.1151,
-    lon: 55.5364,
-    zoom: 11
+  {
+    id: "reunion",
+    name: "Réunion",
+    flag: "🇷🇪",
+    center: { lat: -21.1151, lon: 55.5364 }
   },
-  mayotte: {
-    code: "mayotte",
+  {
+    id: "mayotte",
     name: "Mayotte",
-    lat: -12.8275,
-    lon: 45.1662,
-    zoom: 12
+    flag: "🇾🇹",
+    center: { lat: -12.8275, lon: 45.1662 }
+  },
+  {
+    id: "saintmartin",
+    name: "Saint-Martin",
+    flag: "🇸🇽",
+    center: { lat: 18.0708, lon: -63.0501 }
+  },
+  {
+    id: "saintbarthelemy",
+    name: "Saint-Barthélemy",
+    flag: "🇧🇱",
+    center: { lat: 17.8962, lon: -62.849 }
+  },
+  {
+    id: "caledonie",
+    name: "Nouvelle-Calédonie",
+    flag: "🇳🇨",
+    center: { lat: -22.273, lon: 166.448 }
+  },
+  {
+    id: "polynesie",
+    name: "Polynésie française",
+    flag: "🇵🇫",
+    center: { lat: -17.6797, lon: -149.4068 }
+  },
+  {
+    id: "wallisfutuna",
+    name: "Wallis-et-Futuna",
+    flag: "🇼🇫",
+    center: { lat: -14.2938, lon: -178.1165 }
   }
-};
+];
