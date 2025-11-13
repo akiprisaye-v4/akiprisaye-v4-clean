6f9541d6f7e0abd1c40a0d37d1874a59f6a6b408
teetee971 <thierrynaud2009@gmail.com>
2025-11-13 14:55:51 -0400

    Update territories.js

--- scripts/territories.js
+++ scripts/territories.js
@@ -1,63 +1,99 @@
-// Liste complète des territoires supportés
+// ---------------------------------------------------------
+// territories.js
+// Liste officielle des territoires DOM-TOM
+// avec coordonnées géographiques + niveau de zoom
+// ---------------------------------------------------------
+
 export const TERRITORIES = [
-  {
-    id: "guadeloupe",
-    name: "Guadeloupe",
-    flag: "🇬🇵",
-    center: { lat: 16.265, lon: -61.551 }
-  },
-  {
-    id: "martinique",
-    name: "Martinique",
-    flag: "🇲🇶",
-    center: { lat: 14.6415, lon: -61.0242 }
-  },
-  {
-    id: "guyane",
-    name: "Guyane",
-    flag: "🇬🇫",
-    center: { lat: 4.9224, lon: -52.3135 }
-  },
-  {
-    id: "reunion",
-    name: "Réunion",
-    flag: "🇷🇪",
-    center: { lat: -21.1151, lon: 55.5364 }
-  },
-  {
-    id: "mayotte",
-    name: "Mayotte",
-    flag: "🇾🇹",
-    center: { lat: -12.8275, lon: 45.1662 }
-  },
-  {
-    id: "saintmartin",
-    name: "Saint-Martin",
-    flag: "🇸🇽",
-    center: { lat: 18.0708, lon: -63.0501 }
-  },
-  {
-    id: "saintbarthelemy",
-    name: "Saint-Barthélemy",
-    flag: "🇧🇱",
-    center: { lat: 17.8962, lon: -62.849 }
-  },
-  {
-    id: "caledonie",
-    name: "Nouvelle-Calédonie",
-    flag: "🇳🇨",
-    center: { lat: -22.273, lon: 166.448 }
-  },
-  {
-    id: "polynesie",
-    name: "Polynésie française",
-    flag: "🇵🇫",
-    center: { lat: -17.6797, lon: -149.4068 }
-  },
-  {
-    id: "wallisfutuna",
-    name: "Wallis-et-Futuna",
-    flag: "🇼🇫",
-    center: { lat: -14.2938, lon: -178.1165 }
-  }
+    {
+        id: "all",
+        name: "Tous les territoires",
+        flag: "🌍",
+        center: { lat: 14.6, lon: -53.0 },
+        zoom: 3
+    },
+    {
+        id: "guadeloupe",
+        name: "Guadeloupe",
+        flag: "🇬🇵",
+        center: { lat: 16.265, lon: -61.551 },
+        zoom: 10
+    },
+    {
+        id: "martinique",
+        name: "Martinique",
+        flag: "🇲🇶",
+        center: { lat: 14.6415, lon: -61.0242 },
+        zoom: 10
+    },
+    {
+        id: "guyane",
+        name: "Guyane",
+        flag: "🇬🇫",
+        center: { lat: 4.0, lon: -53.0 },
+        zoom: 7
+    },
+    {
+        id: "reunion",
+        name: "La Réunion",
+        flag: "🇷🇪",
+        center: { lat: -21.1151, lon: 55.5364 },
+        zoom: 10
+    },
+    {
+        id: "mayotte",
+        name: "Mayotte",
+        flag: "🇾🇹",
+        center: { lat: -12.8275, lon: 45.1662 },
+        zoom: 10
+    },
+    {
+        id: "saintmartin",
+        name: "Saint-Martin",
+        flag: "🇫🇷",
+        center: { lat: 18.0708, lon: -63.0501 },
+        zoom: 12
+    },
+    {
+        id: "saintbarthelemy",
+        name: "Saint-Barthélemy",
+        flag: "🇫🇷",
+        center: { lat: 17.9000, lon: -62.8333 },
+        zoom: 12
+    },
+    {
+        id: "saintpierremiquelon",
+        name: "Saint-Pierre-et-Miquelon",
+        flag: "🇫🇷",
+        center: { lat: 46.7794, lon: -56.1773 },
+        zoom: 10
+    },
+    {
+        id: "nouvellecaledonie",
+        name: "Nouvelle-Calédonie",
+        flag: "🇳🇨",
+        center: { lat: -21.299, lon: 165.628 },
+        zoom: 7
+    },
+    {
+        id: "polynesie",
+        name: "Polynésie française",
+        flag: "🇵🇫",
+        center: { lat: -17.6797, lon: -149.4068 },
+        zoom: 9
+    },
+    {
+        id: "wallisfutuna",
+        name: "Wallis-et-Futuna",
+        flag: "🇼🇫",
+        center: { lat: -14.2938, lon: -178.1165 },
+        zoom: 10
+    },
+    {
+        id: "terresautrales",
+        name: "Terres australes",
+        flag: "🇹🇫",
+        center: { lat: -49.3500, lon: 69.1670 },
+        zoom: 5
+    }
 ];
